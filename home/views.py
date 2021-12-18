d93d3c6a44cd8f385fadfbd26f236bc6c34c3fbb
Zehrei Verj Morales <moraleszehreiverj8@gmail.com>
2021-12-18 21:34:58 +0800

    Updated Events

--- home/views.py
+++ home/views.py
@@ -4,13 +4,21 @@ from .models import Chapter, Image, Event
 
 
 def home(request):
+    chapters = Chapter.objects.all()
+    members_sum = sum(chapter.members for chapter in chapters)
+
+    events = [event for event in Event.objects.all()]
+    for event in events:
+        temp_event_date = event.date
+        setattr(event, 'date', temp_event_date.strftime("%B %m, %Y"))
+        setattr(event, 'time', temp_event_date.strftime("%I:%M %p"))
+
     context = {
-        'events': [
-            object for object in Event.objects.all()
-        ],
+        'events': events,
         'images': [
             object.image.url for object in Image.objects.all()
         ],
-        'chapters': Chapter.objects.all()
+        'chapters':  sorted(chapters, key=lambda o: o.barangay),
+        'members_sum': members_sum
     }
     return render(request, 'home/index.html', context)
